5b1bc327a5717e164cd056258303727c3dbf6226
Will Adams <bushblade@gmail.com>
2018-09-15 11:00:18 +0100

    reload content from state if navigated away from page

--- src/components/NewBlog/ContentEditable.vue
+++ src/components/NewBlog/ContentEditable.vue
@@ -3,14 +3,19 @@
     <div class="card">
       <header class="card-header">
         <div class="buttons">
-          <span class="button is-small" v-for="(btn, indx) of commands" :key="indx">
+          <span class="button is-small tooltip" 
+            v-for="(btn, indx) of commands"
+            :key="indx"
+            :data-tooltip="btn.desc"
+            @click="btnAction(btn.cmd, btn.val)">
             <span class="icon">
               <i class="material-icons">{{ btn.icon }}</i>
             </span>
           </span>
         </div>
       </header>
-      <div class="card-content" contenteditable="true">
+      <div id="content-edit" class="card-content content" contenteditable="true"
+      @input="updateContent">
 
       </div>
     </div>
@@ -24,12 +29,28 @@
     name: 'ContentEditable',
     data() {
       return {
-
+        
       }
     },
     computed: {
       commands(){
         return commands
+      }
+    },
+    methods: {
+      btnAction(cmd, val = null){
+        let value = val ? prompt(`Value for ${cmd}?`, val) : ''
+        document.execCommand(cmd, false, value)
+      },
+      updateContent(e){
+        this.content = null 
+        let HTML = e.target.innerHTML
+        this.$store.commit('updateContent', HTML)
+      }
+    },
+    mounted(){
+      if (this.$store.state.newBlog.content){
+        document.getElementById('content-edit').innerHTML = this.$store.state.newBlog.content
       }
     }
   }
