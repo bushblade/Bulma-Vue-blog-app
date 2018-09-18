5f2b2dd72c461f16ae7808a1957d23b6c8b493ae
Will Adams <bushblade@gmail.com>
2018-09-18 10:08:59 +0100

    use firestore id as key

--- src/views/BlogHome.vue
+++ src/views/BlogHome.vue
@@ -13,7 +13,7 @@
       </div>
     </div>
     <transition-group tag="div" class="columns is-multiline" name="card-animation">
-      <div class="column is-one-third-desktop is-half-tablet card-column" v-for="blog in blogs" :key="blog.slug">
+      <div class="column is-one-third-desktop is-half-tablet card-column" v-for="blog in blogs" :key="blog.id">
         <div class="card blog-card">
           <div class="card-image">
             <figure class="image is-5by3">
