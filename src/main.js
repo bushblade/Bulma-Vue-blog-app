cde107c4d9773bd71ed2d8989c15422f5bf271de
Will Adams <bushblade@gmail.com>
2018-09-23 13:04:00 +0100

    added sass for bulma theming

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bulma/css/bulma.css'
 import 'animate.css/animate.css'
+import './assets/sass/styles.scss'
 import 'bulma-tooltip'
 import 'bulma-switch'
-import './assets/styles.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
